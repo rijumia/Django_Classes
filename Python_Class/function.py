dd1f8a5f9e1983cd6dd74996517a55ff2aa603ca
rijumia <riju.mia2612@gmail.com>
2025-06-14 13:24:19 +0600

    Update Python Class 14-04-2025

---
+++ Python_Class/function.py
@@ -0,0 +1,19 @@
+def oddEven(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+oddEven(47)
+
+
+def positiveNegative(num):
+    if num > 0:
+        print("Positive")
+    elif num < 0:
+        print("Negative")
+    else:
+        print("Zero")
+
+positiveNegative(98)
+
