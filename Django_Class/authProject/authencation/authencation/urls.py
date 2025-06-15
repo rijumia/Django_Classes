2bacaa9f1017faa37d231cbd60e1b6606a81bc67
rijumia <riju.mia2612@gmail.com>
2025-06-15 13:33:15 +0600

    User Add

--- Django_Class/authProject/authencation/authencation/urls.py
+++ Django_Class/authProject/authencation/authencation/urls.py
@@ -5,6 +5,6 @@ from userAuth.views import registration, login, home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', registration, name='registration'),
-    path('login/', login, name='login'),
+    path('', login, name='login'),
     path('home/', home, name='home'),
 ]
--- Django_Class/authProject/authencation/userAuth/views.py
+++ Django_Class/authProject/authencation/userAuth/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from userAuth.models import UserAuthModel
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def registration(request):
+    if request.method == 'POST':
+        fullname = request.POST.get('FullName')
+        username = request.POST.get('UserName')
+        email = request.POST.get('Email')
+        contact = request.POST.get('ContactNo')
+        password = request.POST.get('Password')
+        confirm_password = request.POST.get('ConfirmPassword')
+
+        if password == confirm_password:
+            userinfo = UserAuthModel.objects.create(
+            UserName = fullname,
+            FullName = username,
+            Email = email,
+            ContactNo = contact,
+            Password = password
+            )
+            userinfo.save()
+            return redirect('login')
+        else:
+            print("Password Don't Match")
+
     return render(request, 'registration.html')
 
 def login(request):
