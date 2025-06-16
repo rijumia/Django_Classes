77e47160601d2ee9f08bcfa2f7f01a642283d298
rijumia <riju.mia2612@gmail.com>
2025-06-16 13:22:46 +0600

    Image add process

--- Django_Class/Book_Manager/bookstore/books/views.py
+++ Django_Class/Book_Manager/bookstore/books/views.py
@@ -17,38 +17,60 @@ def bookForm(request):
         category = request.POST.get('category')
         publish_date = request.POST.get('publishDate')
         description = request.POST.get('description')
+        cover_image = request.FILES.get('cover')
 
         bookAdd = booksModel.objects.create(
             BookTitle = title,
             BookAuthor = author,
             BookCategory = category,
             BookPublishDate = publish_date,
-            BookDescription = description
+            BookDescription = description,
+            BookCoverPhoto = cover_image,
         )
         bookAdd.save()
         return redirect('bookList')
     return render(request, 'book_form.html',)
 
-def bookUpdate(request, id):
-    books= booksModel.objects.get(id=id)
-    if request.method == 'POST':
-        id = id,
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        category = request.POST.get('category')
-        publish_date = request.POST.get('publishDate')
-        description = request.POST.get('description')
+# def bookUpdate(request, id):
+#     books= booksModel.objects.get(id=id)
+#     if request.method == 'POST':
+#         id = id,
+#         title = request.POST.get('title')
+#         author = request.POST.get('author')
+#         category = request.POST.get('category')
+#         publish_date = request.POST.get('publishDate')
+#         description = request.POST.get('description')
+#         cover_image = request.POST.get('cover')
 
-        bookAdd = booksModel.objects.create(
-            BookTitle = title,
-            BookAuthor = author,
-            BookCategory = category,
-            BookPublishDate = publish_date,
-            BookDescription = description
-        )
-        bookAdd.save()
+
+#         bookAdd = booksModel.objects.create(
+#             BookTitle = title,
+#             BookAuthor = author,
+#             BookCategory = category,
+#             BookPublishDate = publish_date,
+#             BookDescription = description,
+#         )
+#         bookAdd.save()
+#         return redirect('bookList')
+#     return render(request, 'book_update.html', {'books': books})
+def bookUpdate(request, id):
+    books = booksModel.objects.get(id=id)
+    
+    if request.method == 'POST':
+        books.BookTitle = request.POST.get('title')
+        books.BookAuthor = request.POST.get('author')
+        books.BookCategory = request.POST.get('category')
+        books.BookPublishDate = request.POST.get('publishDate')
+        books.BookDescription = request.POST.get('description')
+        
+        if request.FILES.get('cover'):
+            books.BookCoverPhoto = request.FILES.get('cover')
+        
+        books.save()
         return redirect('bookList')
+    
     return render(request, 'book_update.html', {'books': books})
+
 
 def bookDelete(request, id):
      book = booksModel.objects.get(id=id)
--- Django_Class/Book_Manager/bookstore/bookstore/urls.py
+++ Django_Class/Book_Manager/bookstore/bookstore/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from books.views import *
 
 urlpatterns = [
@@ -9,4 +11,4 @@ urlpatterns = [
     path('book_add', bookForm, name='bookAdd'),
     path('book_update/<str:id>/', bookUpdate, name='bookUpdate'),
     path('bookList/<str:id>/', bookDelete, name='bookDelete'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
