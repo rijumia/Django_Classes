85786abb97aae77a598c9577f0d7d350e459dfd3
rijumia <my-email>
2025-06-24 13:17:29 +0600

    continue

--- Django_Class/Custom_User/custom_Users/customUsersApp/admin.py
+++ Django_Class/Custom_User/custom_Users/customUsersApp/admin.py
@@ -3,6 +3,6 @@ from customUsersApp.models import customUser
 from django.contrib.auth.admin import UserAdmin
 
 class userModel(UserAdmin):
-    list_display=['username','email','user_type']
+    list_display=['username','email','user_type','age','mobile','description','instutiteName','passingYear','workExperience','image']
 
 admin.site.register(customUser,userModel)
--- Django_Class/Custom_User/custom_Users/customUsersApp/models.py
+++ Django_Class/Custom_User/custom_Users/customUsersApp/models.py
@@ -7,3 +7,10 @@ class customUser(AbstractUser):
         ('recuiter', 'Recuiter')
     ]
     user_type = models.CharField(choices=role, max_length=10, null=True)
+    age = models.PositiveIntegerField(null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='Midea/img',null=True)
+    instutiteName = models.CharField(max_length=100,null=True)
+    passingYear = models.PositiveIntegerField(null=True)
+    workExperience = models.PositiveSmallIntegerField(null=True)
