8da4b0a793d1a15b20329c09a879aa3b88e67c24
rijumia <my-email>
2025-06-25 11:38:22 +0600

    update 25/06/2026

--- Django_Class/Custom_User/custom_Users/customUsersApp/views.py
+++ Django_Class/Custom_User/custom_Users/customUsersApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from customUsersApp.models import customUser
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -25,6 +25,13 @@ def signupPage(request):
         Password = request.POST.get('Password')
         Confirm_Password = request.POST.get('Confirm_Password')
         role = request.POST.get('role')
+        age = request.POST.get('age')
+        mobile = request.POST.get('mobile')
+        description = request.POST.get('description')
+        instutiteName = request.POST.get('instutiteName')
+        passingYear = request.POST.get('passingYear')
+        workExperience = request.POST.get('workExperience')
+        image = request.FILES.get('image')
 
         if Password == Confirm_Password:
             user = customUser.objects.create_user(
@@ -32,13 +39,57 @@ def signupPage(request):
                 email=Email,
                 password=Confirm_Password,
                 user_type=role,
+                age=age if age else None,
+                mobile=mobile,
+                description=description,
+                instutiteName=instutiteName,
+                passingYear=passingYear if passingYear else None,
+                workExperience=workExperience if workExperience else None,
+                image=image,
             )
             return redirect('loginPage')
         else:
             return render(request, 'signup.html', {'error': 'Passwords do not match!'})
 
     return render(request, 'signup.html')
+def logoutPage(request):
+    logout(request)
+    return redirect('loginPage')
 
 
 def homePage(request):
     return render(request, 'home.html')
+
+def listPage(request):
+    users = customUser.objects.all()
+    return render(request, 'list.html', {'users': users})
+
+def deletePage(request, id):
+    user = customUser.objects.get(id=id)
+    user.delete()
+    return redirect('listPage')
+
+def view_detail(request, id):
+    user = get_object_or_404(customUser, pk=id)
+    return render(request, 'view_detail.html', {'user': user})
+
+def updatePage(request, pk):
+    user = get_object_or_404(customUser, pk=pk)
+
+    if request.method == 'POST':
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.age = request.POST.get('age')
+        user.mobile = request.POST.get('mobile')
+        user.description = request.POST.get('description')
+        user.instutiteName = request.POST.get('instutiteName')
+        user.passingYear = request.POST.get('passingYear')
+        user.workExperience = request.POST.get('workExperience')
+
+        if request.FILES.get('image'):
+            user.image = request.FILES['image']
+
+        user.save()
+        return redirect('resume_detail', pk=user.pk)
+    
+    return render(request, 'update.html', {'user': user})
--- Django_Class/Custom_User/custom_Users/custom_Users/urls.py
+++ Django_Class/Custom_User/custom_Users/custom_Users/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from customUsersApp.views import*
@@ -7,5 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',loginPage, name='loginPage'),
     path('signup/', signupPage, name='signupPage'),
-    path('homePage/', homePage, name='homePage')
-]
+    path('logoutPage/',logoutPage, name='logoutPage'),
+    path('homePage/', homePage, name='homePage'),
+    path('list/', listPage, name='listPage'),
+    path('deletePage/<str:id>', deletePage, name='deletePage'),
+    path('view_detail/<str:id>/', view_detail, name='view_detail'),
+    path('updatePage/<int:pk>/', updatePage, name='updatePage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
