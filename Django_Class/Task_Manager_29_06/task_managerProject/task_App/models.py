77d5dc3a961d79e40acfe25c87977038a7204f00
rijumia <my-email>
2025-06-29 10:09:13 +0600

    More Practice Task Manager Auth and Forenkey Use Mapping

---
+++ Django_Class/Task_Manager_29_06/task_managerProject/task_App/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUserModel(AbstractUser):
+    profile_picture = models.ImageField(upload_to='Media/profile', null=True)
+    bio = models.TextField(null=True)
+    
+class TaskModel(models.Model):
+    user = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE, null=True)
+    TaskTitle = models.CharField(max_length=50, null=True)
+    TaskDescription = models.TextField(null=True)
+    TaskDueDate = models.DateField(null=True)
+    TaskPriority = models.CharField(choices=[('low','Low'),('mediumn','Medium'),('high','High')],max_length=20, null=True)
+    TaskStatus = models.CharField(choices=[('pending','Pending'),('in_progress','In_Progress'),('complated','Complated')],max_length=20, null=True)
+    TaskCreateAt = models.DateTimeField(auto_now_add=True, null=True)
