bd6c3db732657529cb615fd2aac243b1c4f77697
rijumia <my-email>
2025-06-30 10:31:44 +0600

    auto-commit

--- Django_Class/Task_Manager_29_06/task_managerProject/task_App/views.py
+++ Django_Class/Task_Manager_29_06/task_managerProject/task_App/views.py
@@ -42,7 +42,7 @@ def logoutPage(request):
 
 @login_required
 def homePage(request):
-    tasks = TaskModel.objects.filter(user=request.user,TaskStatus="pending")
+    tasks = TaskModel.objects.filter(user=request.user,TaskStatus="in_progress")
     return render(request, 'home.html',{'tasks':tasks})
 
 
